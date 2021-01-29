1bdf95a786647cb44db52721da5db9a839f07615
linkun <linkun@yesoul.com>
2021-01-29 18:03:55 +0800

    第一次提交

---
+++ src/test/java/org/example/Test.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @Description:
+ * @Author: linkun
+ * @date: 2021/1/29 18:03
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("第一次提交");
+    }
+}
